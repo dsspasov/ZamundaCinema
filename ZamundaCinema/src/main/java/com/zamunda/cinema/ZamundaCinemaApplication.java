c83e07018484bd4bb31defe4c45b57421a1b1101
lazarov1994 <lazarov686@gmail.com>
2016-06-26 14:38:26 +0300

    startup project

---
+++ ZamundaCinema/src/main/java/com/zamunda/cinema/ZamundaCinemaApplication.java
@@ -0,0 +1,12 @@
+package com.zamunda.cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZamundaCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZamundaCinemaApplication.class, args);
+	}
+}
